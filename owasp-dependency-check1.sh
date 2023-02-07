78d6c1fa311b523e811d89decc49cdfd6d0b9f14
Deepak <112944445+dsodeepak@users.noreply.github.com>
2023-02-07 11:47:02 +0530

    Update owasp-dependency-check1.sh

--- owasp-dependency-check1.sh
+++ owasp-dependency-check1.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
 DC_VERSION="latest"
 DC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DC_PROJECT="dependency-check scan: $(pwd)"
 DATA_DIRECTORY="$DC_DIRECTORY/data"
 CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
+
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
     mkdir -p "$DATA_DIRECTORY"
@@ -12,6 +14,7 @@ if [ ! -d "$CACHE_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $CACHE_DIRECTORY"
     mkdir -p "$CACHE_DIRECTORY"
 fi
+
 # Make sure we are using the latest version
 docker pull owasp/dependency-check:$DC_VERSION
 
